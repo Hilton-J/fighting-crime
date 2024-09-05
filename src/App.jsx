3c9299b2d32d50855a47e8c18b10ce2d2ed2b553
ShokiMabitsela <shokireshoketswe63@gmail.com>
2024-09-05 15:19:59 +0200

    Shoki's Update 15:18

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import Main from './components/Main.jsx'
 import MainLayout from './layouts/MainLayout.jsx';
 import ViewPage from './pages/ViewPage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
-
+import Wanted from './components/Wanted.jsx';
+import TalkToUs from './components/TalkToUs.jsx';
+import About from './components/about.jsx';
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element={<MainLayout />}>
+      <Route path="/" element={<MainLayout />}>
         <Route index element={<Main />} />
-        <Route path='/view/:id' element={<ViewPage />} />
-        <Route path='*' element={<NotFoundPage />} />
+        <Route path="/view/:id" element={<ViewPage />} />
+        <Route path="/wanted/" element={<Wanted />} />
+        <Route path="/talk/" element={<TalkToUs />} />
+        <Route path="/about/" element={<About />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
-  )
+  );
 
   return <RouterProvider router={router} />
 }
